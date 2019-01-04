e8e69af1f2e85b16868dce0aea26b486b8114665
Erlend Dullaert <erlenddullaert@gmail.com>
2019-01-04 01:46:47 +0100

    converted from JavaFX to WPF .NET Framework

---
+++ Aurora4xJumpCloakCalculator/Aurora4xJumpCloakCalculator/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Aurora4xJumpCloakCalculator.src;
+
+namespace Aurora4xJumpCloakCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Calculator _calculator = new Calculator();
+        private readonly TextBlock _jWeight, _cWeight, _sWeight;
+        private readonly TextBox _jValue, _cValue, _compValue;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _jWeight = (TextBlock) this.FindName("JWeight");
+            _cWeight = (TextBlock) this.FindName("CWeight");
+            _sWeight = (TextBlock) this.FindName("SWeight");
+            _jValue = (TextBox) this.FindName("JumpValue");
+            _cValue = (TextBox) this.FindName("CloakValue");
+            _compValue = (TextBox) this.FindName("CompValue");
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(_compValue.Text, out int compValue))
+                compValue = 0;
+            if (!int.TryParse(_jValue.Text, out int jValue))
+                jValue = 0;
+            if (!int.TryParse(_cValue.Text, out int cValue))
+                cValue = 0;
+
+            int[] result = _calculator.shipCalc(compValue, jValue, cValue);
+            if (result.Length != 3)
+                throw new Exception("Unexpected Array Length");
+            _sWeight.Text = result[2].ToString();
+            _jWeight.Text = result[0].ToString();
+            _cWeight.Text = result[1].ToString();
+        }
+    }
+}
---
+++ Aurora4xJumpCloakCalculator/Aurora4xJumpCloakCalculator/src/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora4xJumpCloakCalculator.src
+{
+    class Calculator
+    {
+        private int calcDrive(int shipWeight, int driveTech)
+        {
+            int driveWeight = (shipWeight / (driveTech - 1));
+            if (driveWeight % 50 != 0) { driveWeight += (50 - driveWeight % 50); }
+
+            return driveWeight;
+        }
+
+        public int[] shipCalc(int initShipWeight, int jumpTech, int cloakTech)
+        {
+            if (cloakTech == 0)
+            {
+                int driveWeight = calcDrive(initShipWeight, jumpTech);
+                //return ("Jump drive Weight:" + driveWeight + " Total Weight:" + (initShipWeight + driveWeight));
+                return new[] {driveWeight, 0, initShipWeight + driveWeight};
+            }
+            else if (jumpTech == 0)
+            {
+                int driveWeight = calcDrive(initShipWeight, cloakTech);
+                //return ("Cloak Weight:" + driveWeight + " Total Weight:" + (initShipWeight + driveWeight));
+                return new[] {0, driveWeight, initShipWeight + driveWeight};
+            }
+            else
+            {
+                int jumpDrive = calcDrive(initShipWeight, jumpTech);
+                int cloakDrive = calcDrive(initShipWeight, cloakTech);
+                int totalShipWeight = initShipWeight + jumpDrive + cloakDrive;
+
+                while ((totalShipWeight / jumpTech) >= jumpDrive || (totalShipWeight / cloakTech) >= cloakDrive)
+                {
+                    while ((totalShipWeight / jumpTech) >= jumpDrive)
+                    {
+                        jumpDrive += 50;
+                        totalShipWeight += 50;
+                    }
+                    while ((totalShipWeight / cloakTech) >= cloakDrive)
+                    {
+                        cloakDrive += 50;
+                        totalShipWeight += 50;
+                    }
+                }
+                //return ("Jump drive Weight:" + jumpDrive + " Cloak Weight:" + cloakDrive + " Total Weight:" + totalShipWeight);
+                return new[] {jumpDrive, cloakDrive, totalShipWeight};
+            }
+        }
+    }
+}
